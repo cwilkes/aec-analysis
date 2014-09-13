e074089859b2e5b78f5c2f228b9392aa72b12e86
Chris Wilkes <cwilkes@gmail.com>
2014-09-13 14:55:27 -0700

    socketio v1

--- run.py
+++ run.py
@@ -1,13 +1,17 @@
 from flask import Flask
-from flask import request
+from flask import request, render_template
+from flask.ext.socketio import SocketIO, emit
 import os
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret'
 DEFAULT_PORT = 5000
+
+socketio = SocketIO(app)
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    return render_template('test.html')
 
 
 @app.route("/", methods=['POST'])
@@ -16,10 +20,30 @@ def post():
     print 'got', d
     return d
 
+@app.route('/t')
+def index():
+    return render_template('test.html')
+
+@socketio.on('my event', namespace='/test')
+def test_message(message):
+    emit('my response', {'data': message['data']})
+
+@socketio.on('my broadcast event', namespace='/test')
+def test_message(message):
+    emit('my response', {'data': message['data']}, broadcast=True)
+
+@socketio.on('connect', namespace='/test')
+def test_connect():
+    emit('my response', {'data': 'Connected'})
+
+@socketio.on('disconnect', namespace='/test')
+def test_disconnect():
+    print('Client disconnected')
+
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', str(DEFAULT_PORT)))
     if port == 0:
         port = DEFAULT_PORT
     host = '0.0.0.0'
-    app.run(port=port, host=host)
+    socketio.run(app, port=port, host=host)
