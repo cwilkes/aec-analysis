236ac2a5ff0c432013b4dd701c93d2fbb9f000fb
Chris Wilkes <cwilkes@gmail.com>
2014-09-13 23:05:06 -0700

    modularizing

---
+++ static/js/app_websocket.js
@@ -0,0 +1,35 @@
+function splitFloats(input) {
+   var data = [];
+    for (var i = 0; i < input.length; i++) {
+        var vals = [];
+        var elements = input[i].split(",");
+        for (var j = 0; j < elements.length; j++) {
+            vals.push(parseFloat(elements[j]));
+        }
+        data.push(vals);
+    }
+    return data;
+}
+
+$(document).ready(function(){
+    namespace = '/data'; // change to an empty string to use the global namespace
+
+    var socket = io.connect('http://' + document.domain + ':' + location.port + namespace);
+    socket.on('connect', function() {
+        socket.emit('my event', {data: 'I\'m connected!'});
+         $('#log').append('<br>connected');
+    });
+
+    var mydict = {};
+    socket.on('data', function(msg) {
+        var channel = msg.channel;
+        var data = splitFloats(msg.data);
+        mydict[channel] = data;
+        loaddata_from_websocket(mydict['nodes'], mydict['bars'], [], mydict['force_nodes'],mydict['force_bars']);
+        render();
+
+    });
+
+
+
+});
